c36c269e80055e32c29a80094763ed7a44e043d2
Thácio <user@MacBook-Pro-de-user.local>
2021-08-10 22:33:17 -0300

    Refatoração da classe pets, conforme novo diagrama.

--- src/main/java/com/Grupo6/CadeMeuPet/config/PetsConfig.java
+++ src/main/java/com/Grupo6/CadeMeuPet/config/PetsConfig.java
@@ -1,24 +1,24 @@
-package com.Grupo6.CadeMeuPet.config;
-
-import com.Grupo6.CadeMeuPet.models.Pets;
-import com.Grupo6.CadeMeuPet.repository.PetsRepository;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import java.sql.Date;
-import java.util.List;
-
-@Configuration
-public class PetsConfig {
-
-    @Bean
-    CommandLineRunner commandLineRunner2(PetsRepository petsRepository){
-        return args -> {
-            Pets pet1 = new Pets("Max","Cachorro","max.png","Masculino","Perdido","Médio","Dobermann",null,null,"Preto",null,null,"Parque Ibirapuera",null,  new Date(System.currentTimeMillis()),Date.valueOf("2021-08-23"));
-            Pets pet2 = new Pets("Mia","Gato","mia.png","Feminino","Achado","Pequeno","Siamês",null,null,"Caramelo",null,null,null,"Rua 4, Bairro Flores", Date.valueOf("2021-05-20"), new Date(System.currentTimeMillis()));
-            petsRepository.saveAll(
-                    List.of(pet1,pet2));
-        };
-    }
-}
+//package com.Grupo6.CadeMeuPet.config;
+//
+//import com.Grupo6.CadeMeuPet.models.Pets;
+//import com.Grupo6.CadeMeuPet.repository.PetsRepository;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import java.sql.Date;
+//import java.util.List;
+//
+//@Configuration
+//public class PetsConfig {
+//
+//    @Bean
+//    CommandLineRunner commandLineRunner2(PetsRepository petsRepository){
+//        return args -> {
+//            Pets pet1 = new Pets("Max","Cachorro","max.png","Masculino","Perdido","Médio","Dobermann",null,null,"Preto",null,null,"Parque Ibirapuera",null,  new Date(System.currentTimeMillis()),Date.valueOf("2021-08-23"));
+//            Pets pet2 = new Pets("Mia","Gato","mia.png","Feminino","Achado","Pequeno","Siamês",null,null,"Caramelo",null,null,null,"Rua 4, Bairro Flores", Date.valueOf("2021-05-20"), new Date(System.currentTimeMillis()));
+//            petsRepository.saveAll(
+//                    List.of(pet1,pet2));
+//        };
+//    }
+//}

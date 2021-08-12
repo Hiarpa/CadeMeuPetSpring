f68ff4b6c9100ca7bb8a37fa575922af8813546d
Hiarpa Netto <hiarpanetto@gmail.com>
2021-08-11 22:13:09 -0400

    JWT parte 2

--- src/main/java/com/Grupo6/CadeMeuPet/security/AuthenticationFilter.java
+++ src/main/java/com/Grupo6/CadeMeuPet/security/AuthenticationFilter.java
@@ -2,16 +2,26 @@ package com.Grupo6.CadeMeuPet.security;
 
 import com.Grupo6.CadeMeuPet.models.UserApp;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import static com.Grupo6.CadeMeuPet.security.SecurityConstants.EXPIRATION_TIME;
+import static com.Grupo6.CadeMeuPet.security.SecurityConstants.KEY;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.security.Key;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
@@ -34,5 +44,14 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
             throw new RuntimeException(e);
         }
 
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest req, HttpServletResponse res, FilterChain chain, Authentication auth) throws IOException, ServletException {
+        Date exp = new Date(System.currentTimeMillis() + EXPIRATION_TIME);
+        Key key = Keys.hmacShaKeyFor(KEY.getBytes());
+        Claims claims = Jwts.claims().setSubject(((org.springframework.security.core.userdetails.User) auth.getPrincipal()).getUsername());
+        String token = Jwts.builder().setClaims(claims).signWith(key, SignatureAlgorithm.HS512).setExpiration(exp).compact();
+        res.addHeader("token", token);
     }
 }

4d5eec0d82c13c24f9e1b3cff607db8a7cfa252e
Hiarpa Netto <hiarpanetto@gmail.com>
2021-08-17 11:20:29 -0400

    fix: userFindById method

--- src/main/java/com/Grupo6/CadeMeuPet/repository/UserRepository.java
+++ src/main/java/com/Grupo6/CadeMeuPet/repository/UserRepository.java
@@ -3,6 +3,7 @@ package com.Grupo6.CadeMeuPet.repository;
 
 import com.Grupo6.CadeMeuPet.models.UserApp;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository

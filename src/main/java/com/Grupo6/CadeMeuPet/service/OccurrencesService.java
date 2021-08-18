50c9e81eccd720da409de1974a25c54c20d8e8b6
Hiarpa Netto <hiarpanetto@gmail.com>
2021-08-18 19:35:58 -0400

    teste : tokenAuth and UserControllerTest

--- src/main/java/com/Grupo6/CadeMeuPet/service/OccurrencesService.java
+++ src/main/java/com/Grupo6/CadeMeuPet/service/OccurrencesService.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.List;
 
 import java.util.Objects;
--- src/test/java/com/Grupo6/CadeMeuPet/test/controller/UserControllerTest.java
+++ src/test/java/com/Grupo6/CadeMeuPet/test/controller/UserControllerTest.java
@@ -6,8 +6,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -16,9 +19,12 @@ import java.sql.Date;
 import java.util.Optional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(UserApp.class)
+@AutoConfigureMockMvc
+@WithMockUser(username = "jhon.caet@gmail.com")
 public class UserControllerTest {
 
     @Autowired
@@ -28,11 +34,25 @@ public class UserControllerTest {
     private UserRepository userRepository;
 
     @Test
-    public void callingUserByWrongIdShouldReturnBadRequest() throws Exception{
+    public void callingUserByWrongIdShouldReturnNotFound() throws Exception{
         Optional<UserApp> userTest = Optional.of(new UserApp(3,"Jhonantan",69473234,"jhon.caet@gmail.com","123praia",9648216,new Date(1999-05-20),"Masculino"));
         Mockito.when(userRepository.findById(5)).thenReturn(userTest);
-        this.mockMvc.perform(get("/api/user")).andExpect(MockMvcResultMatchers.status().isBadRequest());
+        this.mockMvc.perform(
+                get("/api/user")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
 
+    @Test
+    public void shouldReturnJustOneFromResult() throws Exception {
+        UserApp userTest = new UserApp(1,"Jhonantan",69473234,"jhon.caet@gmail.com","123praia",9648216,new Date(1999-05-20),"Masculino");
+        Mockito.when(userRepository.findById(1)).thenReturn(Optional.of(userTest));
+        this.mockMvc.perform(
+                get("/api/user")
+                        .queryParam("idUser","1")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
     }
 
 }
